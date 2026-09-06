b892742e56a8523a53e386043e8dde02df42ebd0
uocgitaobotdangcapmotchut01-spec <uocgitaobotdangcapmotchut01@gmail.com>
2026-09-06 15:23:57 +0700

    Add add function and corresponding tests

--- test_app.py
+++ test_app.py
@@ -1,1 +1,10 @@
+def add(a, b):
+    return a + b
 
+def test_add():
+    assert add(2, 2) == 4
+    assert add(-1, 1) == 0
+
+if __name__ == "__main__":
+    test_add()
+    print("All tests passed.")
